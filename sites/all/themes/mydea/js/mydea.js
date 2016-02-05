a128ad5d3e86b6ca5f8e46c86c0bd16e81925a37
ph3l1x <rperry@mindimage.net>
2016-02-04 23:38:01 -0700

    different version of popover. Still needs Mobile Responsive

--- sites/all/themes/mydea/js/mydea.js
+++ sites/all/themes/mydea/js/mydea.js
@@ -1,9 +1,31 @@
+(function ($) {
+    $(document).ready(function () {
 
+        $('.popAD').click(function (e) {
+            e.preventDefault();
+            var box = $('#popAD');
 
-(function($){
-$(document).ready(function() {
-  
+            if (box.hasClass('servicePopHidden')) {
+                console.log("HAS IT");
+                box.removeClass('servicePopHidden');
+                setTimeout(function () {
+                    box.removeClass('serviceVisuallyHidden');
+                }, 20);
+            }
+        });
 
+        $('.popClose').click(function() {
+
+            var box = $('#popAD');
+
+            box.addClass('serviceVisuallyHidden');
+            box.one('transitionend', function (e) {
+                box.addClass('servicePopHidden');
+            });
+
+        })
+
+    });
 })(jQuery);
 
 
