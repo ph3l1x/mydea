e0b7dc594fa899ac0184442f5099e3ba535294b3
ph3l1x <rperry@mindimage.net>
2016-02-15 20:16:20 -0700

    changes

--- sites/all/themes/mydea/js/mydea.js
+++ sites/all/themes/mydea/js/mydea.js
@@ -23,7 +23,49 @@
                 box.addClass('servicePopHidden');
             });
 
+        });
+
+        /* Make background image fixed when it reaches top of page */
+
+        var distance = $('#block-nodeblock-3').offset().top,
+            $window = $(window);
+        $window.scroll(function() {
+            if($window.scrollTop() >= distance) {
+                $('#block-nodeblock-3').css('background-attachment', 'fixed');
+            } else {
+                $('#block-nodeblock-3').css('background-attachment', 'inherit');
+            }
+        });
+
+        $('.testimonials_section li').hover(function () {
+            $(this).find('.test-image').toggleClass('test-imageHover');
+            $(this).find('.test-whiteBar').toggleClass('hideme');
+        });
+        $('.testimonials_section li').mouseenter( function() {
+            $(this).find('.test-text').css('opacity','1');
+            $(this).find('div.test-image').css('opacity','.4');
+            //$(this).find('img').css({
+            //    'width'     : '130%',
+            //    'max-width' : 'inherit',
+            //    'left'      : '50%',
+            //    'transform' : 'translateX(-50%)',
+            //    'position'  : 'relative'
+            //
+            //});
+        });
+        $('.testimonials_section li').mouseleave(function () {
+            $(this).find('.test-text').css('opacity','0');
+            $(this).find('div.test-image').css('opacity','1');
+            //$(this).find('img').css({
+            //    'width'     : 'inherit',
+            //    'max-width' : 'inherit',
+            //    'left'      : 'inherit',
+            //    'transform' : 'inherit',
+            //    'position'  : 'inherit'
+            //
+            //});
         })
+
 
     });
 })(jQuery);
