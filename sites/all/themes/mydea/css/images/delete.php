dd9bf43f012f3f72657e9a37ab3f68be6d988600
ph3l1x <rperry@mindimage.net>
2016-02-17 23:29:01 -0700

    Added Section 5 and made many other fixes and adjustments

---
+++ sites/all/themes/mydea/css/images/delete.php
@@ -0,0 +1,358 @@
+==============================================
+===== About Section Page ==============
+==============================================
+<div class="sections about_section">
+  <div class="about-content">
+    <p>
+      We pride ourselves and our continued success on the ability to attract and
+      retain top-level talent and personalities that creates an office
+      atmosphere that continues to foster and
+      encourage new, innovative ideas to bring to our customers and projects.
+      With over 50 years of combined experience focused on business development,
+      market strategy and growth of our
+      customers, our talent and experience is our differentiator in the market
+      and is our most valued asset.
+    </p>
+    <p>
+      Located in Eagle, ID, just outside Idaho’s capital city of Boise, we are
+      continually ranked at the Top of the list of America’s Best Cities to live
+      in. Low crime rate, affordable cost of
+      living, great universities (Go Broncos) and a fast growing job market have
+      attracted and retained top young minds and talent. - Learn more about
+      Boise ID and why we are all so proud to
+      call it home.
+    </p>
+    <div class="about-download redButton"><a target="_BLANK"
+                                             href="/sites/default/files/Regional-Overview---General-1.pdf">Boise
+        Metro Regional Overview</a></div>
+  </div>
+  <div>
+    <ul>
+      <li class="first">
+        <div class="about-image"><img src="/sites/default/files/test1.jpg"/>
+        </div>
+        <div class="about-text">
+          Dustin joins Mydea Marketing with a degree in Business Communications
+          from Boise State. He brings insight in process optimization, warehouse
+          and inventory management and new
+          technologies integration. Dustin sets the tone for work ethic and
+          drive for MyDea Marketing. He enjoys all the outdoors that Idaho has
+          to offer and spends his falls hunting and
+          fishing in our beautiful mountains.
+          <div class="about-more"><a href="#" id="magicPopUp"
+                                     class="popAboutDustin">more</a></div>
+        </div>
+        <div class="about-whiteBar hideme">Dustin Bevins</div>
+      </li>
+      <li class="second">
+        <div class="about-image"><img alt=""
+                                      src="/sites/default/files/test2.jpg"/>
+        </div>
+        <div class="about-text">
+          As a graduate of Boise State with a degree in Business Management,
+          Jeff is the founder of MyDea Marketing. Jeff’s experience in sales
+          helped him to shape the understanding needed
+          to create a successful demand generation company. Jeff spends his free
+          time in the mountains of Idaho enjoying biking, hiking and
+          snowboarding whenever possible.
+          <div class="about-more"><a href="#" id="magicPopUp"
+                                     class="popAboutJeff">more</a></div>
+        </div>
+        <div class="about-whiteBar hideme">Jeff McCoy</div>
+      </li>
+      <li class="third">
+        <div class="about-image"><img alt=""
+                                      src="/sites/default/files/test3.jpg"/>
+        </div>
+        <div class="about-text">
+          Porter joined MyDea Marketing in early January 2015 at the early age
+          of 5.5 weeks old. Since joining the team, Porter has consistently set
+          the standard for attendance and brings a
+          relaxed attitude with him every day to work. His primary job
+          responsibility is monitoring overall office cleanliness. He routinely
+          checks everyone’s trash cans and is the first to
+          take anyone’s unwanted lunch. Porter also works as security clearance
+          for visitors and guests. His favorite hobby is fetching anything and
+          everything and I think we can all agree
+          that the office wouldn’t be the same without him.
+          <!--  <div class="about-more"><a href="#" id="magicPopUp" class="popAboutDustin">more</a></div> -->
+        </div>
+        <div class="about-whiteBar hideme">Porter</div>
+      </li>
+    </ul>
+  </div>
+</div>
+==============================
+==== Contact Section Page ====
+==============================
+
+<div class="sections contact_section">
+  <div class="contact-header">
+    <h2>Contact Us</h2>
+    <div class="contact-headerContent">
+      Thank you for your interest in MyDea. You can call, or complete and send
+      the web form to contact us. We'll contact you as soon as possible to start
+      our collaboration to increase your customer base.
+    </div>
+  </div>
+  <div class="footer-contactContainer">
+    <div class="footer contact-logo">
+      <img src="/sites/default/files/mydea_logo_white.png">
+      <div class="footer-socialMedia">
+        <div class="facebookx socialMedia"><a target="_BLANK" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></div>
+        <div class="twitterx socialMedia"><a target="_BLANK" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></div>
+        <div class="googleplusx socialMedia"><a target="_BLANK" href="http://www.googleplus.com"><i class="fa fa-google-plus"></i></a></div>
+        <div class="linkedinx socialMedia"><a target="_BLANK" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></div>
+      </div>
+    </div>
+
+    <div class="footer contact-about">
+      <div class="contac-aboutTitle">Call us:</div>
+      <div class="contact-aboutPhone">208.853.1776</div>
+      <div class="contact-aboutAddress">
+        Mydea Marketing<br/>
+        1234 W. State St<br/>
+        Boise, ID 83642
+      </div>
+    </div>
+
+    <div class="footer contact-form">
+      <?php
+      $node = node_load(7);
+      webform_node_view($node, 'full');
+      print theme_webform_view($node->content);
+      ?>
+    </div>
+  </div>
+</div>
+
+===========================================
+=== Testimonials Section Page ===
+=========================================
+
+<div class="sections testimonials_section">
+  <div class="boxes-2">
+    <ul>
+      <li>
+        <div class="test-content">
+          <i class="fa fa-quote-left"></i>Mydea is a high-value partner. I've
+          worked with them while leading account development teams at two
+          separate organizations over the past five years and have consistently
+          received best-in-class service from their customer and prospect
+          engagement services. We've been able to leverage the information that
+          they've uncovered from customer engagement activities to win business
+          and strengthen customer relationship. Mydea delivers.<i
+            class="fa fa-quote-right"></i>
+          <div class="test-title"> – Senior Manager, Market Development</div>
+        </div>
+      </li>
+      <li>
+        <div class="test-content">
+          <i class="fa fa-quote-left"></i>The MyDea Marketing team has been a
+          huge asset and an important factor in the growth of new business
+          segments at our company. The individuals that I have worked with bring
+          a high level of commitment and have become an integral part of our
+          team. MyDea has helped rapidly grow the pipeline of prospects and
+          assist with many other aspects of building market awareness. I would
+          highly recommend MyDea Marketing to any organization looking to
+          penetrate new markets or scale their business.<i
+            class="fa fa-quote-right"></i>
+          <div class="test-title"> – Senior Manager, New Segments</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+
+============================================
+=== Services Section Page ===============
+==========================================
+<div class="sections services_section">
+  <h1>Offering Helpful Business<br/>
+    Tips &amp; Advice to help</h1>
+
+  <h2>your business <span>start up</span></h2>
+
+  <div class="boxes-6">
+    <ul>
+      <li>
+        <div class="box-title">Account Development</div>
+
+        <div class="services-box-text">Projects for the development of corporate
+          strategy include: The formation of visions of the company, statement
+          of corporate objectives, weighing of the
+          potential, the formation of conditions.
+        </div>
+
+        <hr/>
+        <div class="services-box-readMore"><a id="magicPopUp" class="popAD"
+                                              href="#">read more</a></div>
+      </li>
+      <li>
+        <div class="box-title">Webinar/Seminar Recruitment</div>
+
+        <div class="services-box-text">Projects for the development of corporate
+          strategy include: The formation of visions of the company, statement
+          of corporate objectives, weighing of the
+          potential, the formation of conditions.
+        </div>
+
+        <hr/>
+        <div class="services-box-readMore"><a id="magicPopUp" class="popWSR"
+                                              href="#">read more</a></div>
+      </li>
+      <li>
+        <div class="box-title">Events/Tradeshow</div>
+
+        <div class="services-box-text">Projects for the development of corporate
+          strategy include: The formation of visions of the company, statement
+          of corporate objectives, weighing of the
+          potential, the formation of conditions.
+        </div>
+
+        <hr/>
+        <div class="services-box-readMore"><a id="magicPopUp" class="popET"
+                                              href="#">read more</a></div>
+      </li>
+      <li>
+        <div class="box-title">Sales Inbound/Outsourcing</div>
+
+        <div class="services-box-text">Projects for the development of corporate
+          strategy include: The formation of visions of the company, statement
+          of corporate objectives, weighing of the
+          potential, the formation of conditions.
+        </div>
+
+        <hr/>
+        <div class="services-box-readMore"><a id="magicPopUp" class="popSIO"
+                                              href="#">read more</a></div>
+      </li>
+      <li>
+        <div class="box-title">Channel Development</div>
+
+        <div class="services-box-text">Projects for the development of corporate
+          strategy include: The formation of visions of the company, statement
+          of corporate objectives, weighing of the
+          potential, the formation of conditions.
+        </div>
+
+        <hr/>
+        <div class="services-box-readMore"><a id="magicPopUp" class="popCD"
+                                              href="#">read more</a></div>
+      </li>
+      <li>
+        <div class="box-title">Data Service</div>
+
+        <div class="services-box-text">Projects for the development of corporate
+          strategy include: The formation of visions of the company, statement
+          of corporate objectives, weighing of the
+          potential, the formation of conditions.
+        </div>
+
+        <hr/>
+        <div class="services-box-readMore"><a id="magicPopUp" class="popDS"
+                                              href="#">read more</a></div>
+      </li>
+    </ul>
+  </div>
+
+  <div class="services-info"><a href="#">Account Development</a> - <a href="#">Webiniar/Seminiar
+      Recruitment</a> - <a href="#">Events/Tradeshow</a> - <a href="#">Sales
+      Inbound/Outsourcing</a> - <a
+      href="#">Channel Development</a> - <a href="#">Data Service</a></div>
+
+  <div class="services-readMore"><a href="#">read more</a></div>
+</div>
+
+
+=======================================================
+BLOCKS for About Section
+========================================================
+
+
+<div class="servicesPop servicePopHidden serviceVisuallyHidden"
+     id="popAboutDustin">
+  <div class="servicesPopInner">
+    <div class="servicesPopInnerContent"><img class="popClose"
+                                              src="sites/all/themes/mydea/css/images/close-icon.png"/>
+      <h1>Dustin Bevins</h1>
+      <div class="popover-p">
+        Dustin Bevins graduated Boise State University with a degree in Business
+        and Communications after growing up in rural Idaho. Dustin had the
+        opportunity to influence
+        many different business processes in his career at a top grocer company.
+        With involvement in multiple aspects of purchasing, warehouse and
+        process optimization continuing into a
+        warehouse management role. Dustin was a crucial part of the team that
+        investigated new technologies and choosing to implement voice in their
+        warehouses. As such he was involved in
+        ensuring the new technologies achieved optimization goals as well as
+        boosting existing employee morale. He continued to support
+        implementation processes and post implementation
+        training and resource management. After leaving his career in grocery he
+        chose to join the growing MyDea Marketing team taking on a crucial role
+        as an Account Manager for multiple
+        customers across several industry verticals. He is a leader in
+        implementing new and insightful processes to ensure our teams continued
+        success. Dustin’s unrelenting work ethic and
+        inability to be defeated continues to set the tone for all those who
+        work with and around him. Dustin leverages his experiences in multiple
+        roles to ensure that we continue to be a
+        leader in all segments of business.
+      </div>
+    </div>
+  </div>
+</div>
+
+=====================================================================================
+BLOCKS for Services Section
+====================================================================================
+
+
+<div class="servicesPop servicePopHidden serviceVisuallyHidden" id="popAD">
+  <div class="servicesPopInner">
+    <div class="servicesPopInnerContent">
+      <img class="popClose"
+           src="sites/all/themes/mydea/css/images/close-icon.png">
+      <h1>Account Development</h1>
+      <hr/>
+      <div class="popover-p">
+        The core of our business is account development for a solution sale. We
+        create the desire, understand prospects’ processes and operations at a
+        detailed level, and develop the
+        urgency.
+        As a result, we provide a qualified prospect to sales/partners so they
+        can stay focused on what they do best- close business.
+      </div>
+      <ul>
+        <li>Validate/update/scrub current company list ensuring companies/titles
+          match prospect profile
+        </li>
+        <li>Develop new data driven prospecting lists</li>
+        <li>Manage strategic accounts – nurture your top identified prospects
+        </li>
+        <li>Develop qualification metrics and understand
+          product/solution/market
+        </li>
+        <li>Find the correct decision maker and make contact by phone, email or
+          other marketing tactics
+        </li>
+        <li>Establish relationships to understand the prospect’s needs and pain
+          points. Identify specific processes and where your services can
+          provide business value
+        </li>
+        <li>Create success measurement tracking/reporting on all activity</li>
+        <li>Seamlessly transfer the relationship with the prospect to your sales
+          team - we appear as an extension of your sales department
+        </li>
+        <li>Nurture accounts/contacts that due to a variety of reasons aren’t
+          ready to engage
+        </li>
+        <li>Follow up to ensure no opportunities are dropped</li>
+        <li>Transparently report in CRM</li>
+        <li>Create content for mass email/drip campaigns</li>
+
+      </ul>
+    </div>
+  </div>
+</div>
